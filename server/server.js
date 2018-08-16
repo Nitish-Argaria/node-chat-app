ea979a9ae661f6d757b9f1b493519b8dea377d6a
argaria <argaria.nitish@gmail.com>
2018-08-16 11:53:00 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'/../public');
+//console.log(__dirname+'/../public');
+//console.log(publicPath);
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+// app.get('/',(req,res)=>{
+// 	res.render('./../public/index.html');
+// });
+
+
+app.listen(port,()=>{
+	console.log("app has been started on port",3000);
+});
+
+
+
