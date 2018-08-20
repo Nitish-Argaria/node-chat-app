0704fe9c24ddc2a437a486739cf288082be4bf6b
argaria <argaria.nitish@gmail.com>
2018-08-20 17:30:08 +0530

    Scroll ti bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,24 @@
 var socket = io();
+
+function scrollToBottom () {
+	//selectos
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+	if(clientHeight+scrollTop +newMessageHeight+ lastMessageHeight >= scrollHeight)
+	{
+		messages.scrollTop(scrollHeight);
+	}
+
+}
+
+
+
 socket.on('connect',function(argument) {
 	// body...
 
@@ -30,6 +50,7 @@ socket.on('newMessage',function(message){
 		createdAt:formatedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 // socket.on('newLocationMessage', function (message) {
@@ -60,6 +81,7 @@ socket.on('newLocationMessage', function (message) {
   	url:message.url
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit',function(e) {
